fa594e10c2e5fe94ccb8e0aad6986f690ab84f33
Julie Zhou <zz505000@gmail.com>
2018-07-05 00:32:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var greeting = "Hello, everybody!";
 
 var specialGuest = "Neil deGrasse Tyson"
 
-var greetSpecialGuest = "Hello," + specialGuest + "!";
+var greetSpecialGuest = "Hello, " + specialGuest + "!";
 
 var space = "Let\'s talk about space."
 
